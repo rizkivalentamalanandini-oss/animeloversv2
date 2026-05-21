ad50354fad4049fa2fe72458551d6119f146e118
rizkivalentamalanandini-oss <rizkivalentamalanandini@gmail.com>
2026-05-21 09:41:24 +0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -4,24 +4,20 @@ function loginUser() {
   let message = document.getElementById("message");
   let alertBox = document.getElementById("alertBox");
 
-  // Kosong
   if (username === "" || password === "") {
     message.style.display = "block";
     message.innerText = "Username dan password wajib diisi!";
     return;
   }
 
-  // Login sesuai guru
-  if (username === "heri" && password === "123") {
+  if (username === "heri" && password === "123")
+  if (username === "valent" && password === "123") {
 
-    // Simpan username
     localStorage.setItem("username", username);
 
-    // Tampilkan alert sukses
     alertBox.style.display = "block";
     alertBox.innerText = "Login berhasil!";
 
-    // Pindah halaman
     setTimeout(() => {
       window.location.href = "profile.html";
     }, 1000);
